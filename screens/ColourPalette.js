0716aeae4f61b493ca369592f3bdaab43309e89f
unknown <tombirbeck47@gmail.com>
2023-01-23 16:18:49 +0000

    set colour palette to be rendered using route.params

--- screens/ColourPalette.js
+++ screens/ColourPalette.js
@@ -2,35 +2,18 @@ import React from "react";
 import { FlatList, Text, StyleSheet} from 'react-native'
 import ColourBox from "../components/ColourBox";
 
-const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
 
-const ColourPalette = () => {
+const ColourPalette = ({ route }) => {
+    const {colours, paletteName} = route.params
     return (
         <FlatList
           style={styles.container}
-          data={COLORS}
+          data={colours}
           keyExtractor={(item) => item.hexCode}
           renderItem={({ item }) => (
             <ColourBox hexCode={item.hexCode} colorName={item.colorName} />
           )}
-          ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
+          ListHeaderComponent={<Text style={styles.heading}>{paletteName}</Text>}
         />
     )
 }
